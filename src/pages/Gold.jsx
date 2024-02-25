09a0b57764a3611903c0e4ee8cd378c7a76cc463
Sameer <sameer4363@gmail.com>
2024-02-25 10:09:34 -0400

    Add quickEditGold form

--- src/pages/Gold.jsx
+++ src/pages/Gold.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { fetchGold, quickEditGold, addTransaction } from '../apiClient';
 import NotLoggedIn from '../components/NotLoggedIn';
+import Errors from '../components/Errors';
 import TransactionForm from '../components/TransactionForm';
 
 const dateOptions = {
@@ -22,7 +23,9 @@ const Gold = (props) => {
   const [gold, setGold] = useState(0);
   const [transactions, setTransactions] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState([]);
   const [showTransactionForm, setShowTransactionForm] = useState(false);
+  const [showQuickEditGold, setShowQuickEditGold] = useState(false);
 
   const getGold = async (id) => {
     try {
@@ -36,15 +39,16 @@ const Gold = (props) => {
 
   const handleQuickEditGold = async (newQuantity) => {
     try {
-      const response = await quickEditGold(userId, {
-        newQuantity,
-      });
+      const response = await quickEditGold(userId, newQuantity);
       console.log('Gold changed successfully', response);
+      setShowQuickEditGold(false);
       // await getGold();
       setGold(response.quantity);
       setTransactions(response.transactions);
+      setErrors([]);
     } catch (error) {
-      console.error('Error changing gold: ', error);
+      console.error('Error quick editing gold: ', error.response?.data);
+      setErrors(error.response?.data?.errors?.map((err) => err.msg));
     }
   };
 
@@ -56,8 +60,10 @@ const Gold = (props) => {
       // await getGold();
       setGold(response.quantity);
       setTransactions(response.transactions);
+      setErrors([]);
     } catch (error) {
-      console.error('Error adding transaction: ', error);
+      console.error('Error adding transaction: ', error.response?.data);
+      setErrors(error.response?.data?.errors?.map((err) => err.msg));
     }
   };
 
@@ -75,12 +81,30 @@ const Gold = (props) => {
       {!isLoggedIn && <NotLoggedIn />}
       {isLoggedIn && (
         <>
-          <h2>{gold}</h2>
+          {!showQuickEditGold ? (
+            <h2>{gold}</h2>
+          ) : (
+            <QuickEditGoldForm
+              initialQuantity={gold}
+              onQuickEditGoldSubmit={handleQuickEditGold}
+            />
+          )}
           <div>
-            <button type="button">Quick Adjust Gold</button>
             <button
               type="button"
-              onClick={() => setShowTransactionForm((prevState) => !prevState)}
+              onClick={() => {
+                setShowQuickEditGold((prevState) => !prevState);
+                setShowTransactionForm(false);
+              }}
+            >
+              Quick Adjust Gold
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setShowTransactionForm((prevState) => !prevState);
+                setShowQuickEditGold(false);
+              }}
             >
               Create new transaction
             </button>
@@ -90,6 +114,7 @@ const Gold = (props) => {
                 initialQuantity={gold}
               />
             )}
+            {errors.length > 0 && <Errors errors={errors} />}
             <div className="transaction-container">
               <h2>Previous Transactions</h2>
               {transactions.length > 0 ? (
@@ -112,6 +137,32 @@ const Gold = (props) => {
         </>
       )}
     </main>
+  );
+};
+
+const QuickEditGoldForm = (props) => {
+  const { initialQuantity, onQuickEditGoldSubmit } = props;
+
+  const [quantity, setQuantity] = useState(initialQuantity || 0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onQuickEditGoldSubmit(quantity);
+  };
+
+  return (
+    <form className="quickEditGold-form" onSubmit={handleSubmit}>
+      <label htmlFor="quantity">
+        New Quantity:
+        <input
+          type="number"
+          name="quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+      </label>
+      <button type="submit">Submit New Quantity</button>
+    </form>
   );
 };
 
