8c281f41c3a1f248cd2cc219d08814b540bf90cd
Sameer <sameer4363@gmail.com>
2024-04-10 17:31:45 -0400

    Change baseURL from localhost to vercel link

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  baseURL: 'http://localhost:3000',
-  // baseURL: 'https://inventory-api-iota.vercel.app/',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://inventory-api-iota.vercel.app/',
 };
 
 export default config;
